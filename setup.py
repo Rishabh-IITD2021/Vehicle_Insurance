d68dd466957c20d7bc26c7653cd5a5c2104e05cf
Rishabh Porwal <risjain201@gmail.com>
2025-05-18 00:55:40 +0530

    added the structure of the project and logger and expectional handling

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Rishabh Porwal",
+    author_email="risjain201@gmail.com",
+    packages=find_packages()
+)
